15b1bfba7d26d09a16f8ebb0c02860590c4a1465
satoshiwasHere <skinyanjui100@gmail.com>
2023-05-31 02:53:21 -0700

    Binary tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,20 +10,20 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *new;
 
 	if (value == '\0')
 
 		return (NULL);
 
-	node = malloc(sizeof(binary_tree_t));
+	new = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
 
-	node->parent = parent;
-	node->n = value;
-	node->right = NULL;
-	node->left = NULL;
+	new>parent = parent;
+	new->n = value;
+	new->right = NULL;
+	new->left = NULL;
 
-	return (node);
+	return (new);
 }
