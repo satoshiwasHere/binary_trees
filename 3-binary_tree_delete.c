dca8847c594a7f4c63103b5c34f33a3cc5b30753
satoshiwasHere <skinyanjui100@gmail.com>
2023-05-31 09:30:39 -0700

    b_tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,14 +3,15 @@
 /**
  * binary_tree_delete - function deletes a binary tree.
  * @tree: points to the root node of the tree to delete.
+ *
  * descp: do nothing if tree = NULL
  */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if(tree is not equal to NULL)
-	{
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
-	}
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
