e09183c8ac20ef8dfa3be28ed34f085086fbdac1
satoshiwasHere <skinyanjui100@gmail.com>
2023-05-31 12:41:57 -0700

    b_tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,3 +1,5 @@
+#include "binary_trees.h"
+
 /**
  * binary_tree_size - evaluates the size of a binary tree
  * @tree: pointer to binary root tree node
@@ -5,6 +7,7 @@
  * Return: size of the tree, 0 - null
  *
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	return ((tree) ? (binary_tree_size(tree->left) +
