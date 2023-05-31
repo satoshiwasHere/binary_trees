f6a64b7ac28a1aef6291f1ee2f7d091f934230ec
satoshiwasHere <skinyanjui100@gmail.com>
2023-05-31 10:02:05 -0700

    b_tree

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,8 +8,7 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node && !node->left && !node->right)
+	if (node == NULL || node->left != NULL || node->right != NULL)
 		return (0);
-
 	return (1);
 }
