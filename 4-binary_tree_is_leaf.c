5b2cf09a6d37ad9d4fc4b444f5dba4a7c33fd456
satoshiwasHere <skinyanjui100@gmail.com>
2023-05-31 10:14:26 -0700

    b_tree

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,12 +3,14 @@
 /**
  * binary_tree_is_leaf - function checks if a node is a leaf
  * @node: node to be checked
- * return: if node is a leaf - 1, alternitely - 0
+ *
+ * Return: 1 if node is a leaf,
+ * 0 if not a leaf or NULL
  */
-
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL || node->left != NULL || node->right != NULL)
 		return (0);
+
 	return (1);
 }
